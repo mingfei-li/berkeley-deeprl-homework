7aeadf268032bce90ee9dcf233d854535b1186b0
Mingfei Li <ml@mingfei.io>
2025-03-10 19:33:29 -0700

    hw1 (add visualization code + more experiments)

--- hw1/q1_run.sh
+++ hw1/q1_run.sh
@@ -1,7 +1,13 @@
 #!/bin/zsh
 
-python cs285/scripts/run_hw1.py \
-        --expert_policy_file cs285/policies/experts/$1.pkl \
-        --env_name $1-v4 --exp_name bc_${1:l} --n_iter 1 \
-        --eval_batch_size 5000 \
-        --expert_data cs285/expert_data/expert_data_$1-v4.pkl
+for n_steps ({1000..30000..1000}) {
+        echo "n_steps = $n_steps" >> log
+        python cs285/scripts/run_hw1.py \
+                --expert_policy_file cs285/policies/experts/$1.pkl \
+                --env_name $1-v4 --exp_name bc_${1:l} --n_iter 1 \
+                --eval_batch_size 5000 \
+                --expert_data cs285/expert_data/expert_data_$1-v4.pkl \
+                --video_log_freq -1 \
+                --train_batch_size 1000 \
+                --num_agent_train_steps_per_iter $n_steps >> log
+}
---
+++ hw1/visualize.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('log')
+x = df['n_steps']
+y = df['Eval_AverageReturn']
+error = df['Eval_StdReturn']
+plt.plot(x, y, 'b-', label='Data')
+plt.fill_between(x, y - error, y + error, alpha=0.3, color='b', label='Error')
+plt.xlabel('num_agent_train_steps_per_iter')
+plt.ylabel('Eval_AverageReturn')
+plt.savefig('bc_hyperparameter_tuning.jpg')
