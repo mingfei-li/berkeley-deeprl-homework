c51166eb63ede5a969d11fb1cd61fe7a009970e3
Mingfei Li <ml@mingfei.io>
2025-04-24 23:28:58 -0700

    hw4 p1

--- hw4/cs285/agents/model_based_agent.py
+++ hw4/cs285/agents/model_based_agent.py
@@ -89,7 +89,12 @@ class ModelBasedAgent(nn.Module):
         # directly
         # HINT 3: make sure to avoid any risk of dividing by zero when
         # normalizing vectors by adding a small number to the denominator!
-        loss = ...
+
+        obs_acs = (torch.cat([obs, acs], dim=1) - self.obs_acs_mean) / (self.obs_acs_std + 1e-6)
+        deltas = (next_obs - obs - self.obs_delta_mean) / (self.obs_delta_std + 1e-6)
+
+        preds = self.dynamics_models[i](obs_acs)
+        loss = self.loss_fn(preds, deltas)
 
         self.optimizer.zero_grad()
         loss.backward()
@@ -111,10 +116,12 @@ class ModelBasedAgent(nn.Module):
         acs = ptu.from_numpy(acs)
         next_obs = ptu.from_numpy(next_obs)
         # TODO(student): update the statistics
-        self.obs_acs_mean = ...
-        self.obs_acs_std = ...
-        self.obs_delta_mean = ...
-        self.obs_delta_std = ...
+        obs_acs = torch.cat([obs, acs], dim=1)
+        self.obs_acs_mean = torch.mean(obs_acs, dim=0, keepdim=True)
+        self.obs_acs_std = torch.std(obs_acs, dim=0, keepdim=True)
+        deltas = next_obs - obs
+        self.obs_delta_mean = torch.mean(deltas, dim=0, keepdim=True)
+        self.obs_delta_std = torch.std(deltas, dim=0, keepdim=True)
 
     @torch.no_grad()
     def get_dynamics_predictions(
@@ -219,5 +226,6 @@ class ModelBasedAgent(nn.Module):
                 # TODO(student): implement the CEM algorithm
                 # HINT: you need a special case for i == 0 to initialize
                 # the elite mean and std
+                pass
         else:
             raise ValueError(f"Invalid MPC strategy '{self.mpc_strategy}'")
--- hw4/cs285/scripts/run_hw4.py
+++ hw4/cs285/scripts/run_hw4.py
@@ -119,7 +119,12 @@ def run_training_loop(
         if itr == 0:
             # TODO(student): collect at least config["initial_batch_size"] transitions with a random policy
             # HINT: Use `utils.RandomPolicy` and `utils.sample_trajectories`
-            trajs, envsteps_this_batch = ...
+            trajs, envsteps_this_batch = utils.sample_trajectories(
+                env,
+                utils.RandomPolicy(env),
+                config["initial_batch_size"],
+                ep_len,
+            )
         else:
             # TODO(student): collect at least config["batch_size"] transitions with our `actor_agent`
             trajs, envsteps_this_batch = ...
@@ -165,6 +170,15 @@ def run_training_loop(
             # TODO(student): train the dynamics models
             # HINT: train each dynamics model in the ensemble with a *different* batch of transitions!
             # Use `replay_buffer.sample` with config["train_batch_size"].
+            for i in range(mb_agent.ensemble_size):
+                sample = replay_buffer.sample(config["train_batch_size"])
+                loss = mb_agent.update(
+                    i,
+                    sample["observations"],
+                    sample["actions"],
+                    sample["next_observations"],
+                )
+                step_losses.append(loss)
             all_losses.append(np.mean(step_losses))
 
         # on iteration 0, plot the full learning curve
